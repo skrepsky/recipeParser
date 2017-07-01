129f3754c409c701a5bfa7585a0a1cf6329b8df1
skrepsky <scott.k.krepsky@gmail.com>
2017-07-01 11:40:21 -0500

    update recipeParser and remove UnitResult

--- Classes/UnitResult.cs
+++
@@ -1,10 +0,0 @@
-using System; using System.Collections.Generic; using System.IO; using System.Linq; using System.Text; using System.Net; using Newtonsoft.Json; using Newtonsoft.Json.Linq; using System.Text.RegularExpressions;  namespace recipeParser { 
-public class UnitResult : IComparable<UnitResult>     {         public UnitResult(string name){             this.Name = name;             this.Count = 0;           }           public string Name         { get; set;}          public int Count
-	{ get; set;}
-
-        public int CompareTo(UnitResult other)
-        {
-            if (other.Count > this.Count)
-            {                 return -1;             }             else if (other.Count == this.Count)
-            {                 return 0;             }             else return 1;
-        }     } }
